cd9a2f9a6a816bfe495f1d63e35f598cc938eb44
lubavas <19555010+lubavas@users.noreply.github.com>
2023-03-09 16:24:25 +0100

    RT-aligning functionality
    to check for movement- and perception specific components

--- gfs_prep_data_to_convert_NDT_raster.m
+++ gfs_prep_data_to_convert_NDT_raster.m
@@ -1,7 +1,7 @@
 clear all, close all
 
 % This script prepares data to be converted into NDT-eatable format and
-% saves them into di
+% saves them into separate folders for respective data sets
 
 currDir = 'C:\Users\lvasileva\Documents\Luba\scripts\ndt_prep\';
 cd(currDir)
@@ -18,6 +18,8 @@ for bandNum = 1:length(band_list)
 end
 
 dataTypes = {'SPKSEL', 'BLP'};
+
+shift_time = 450; % time point to which the code aligns the reaction time in each trial
 
 % list of the needed fields
 fieldList = ...
@@ -94,6 +96,10 @@ for ii = 1:length(sessionNames)
             
         end
         
+        % create fake RTs from corresponding disap trials
+        TargBotheyes_fake = randsample(dt.LFPSEL.RPL.CatchTargRemov, size(dt.LFPSEL.TargBotheyes, 3), true); % physical condition
+        nocatch_nodisap_fake = randsample(dt.LFPSEL.RPL.nc_disap, size(dt.LFPSEL.nocatch_nodisap, 3), true); % ambiguous condition
+        
         % loop through channels
         for chNum = 1:size(dt.SPKSEL.TargBotheyes, 2)
             
@@ -118,17 +124,28 @@ for ii = 1:length(sessionNames)
             SPK.AddInfo = dt.SPKSEL.AddInfo;
             SPK.RPL = dt.LFPSEL.RPL;
             
+            % add the randomized RTs into the data structure
+            SPK.RPL.TargBotheyes_fake = TargBotheyes_fake;
+            SPK.RPL.nocatch_nodisap_fake = nocatch_nodisap_fake;
+            
             % create spike RT-shifted data
             for trNum = 1:size(SPK.CatchTargRemov, 2)
-                SPK.CatchTargRemov_shifted(:, trNum) = circshift(SPK.CatchTargRemov(:, trNum), 350 - SPK.RPL.CatchTargRemov(trNum));
+                SPK.CatchTargRemov_shifted(:, trNum) = circshift(SPK.CatchTargRemov(:, trNum), shift_time - SPK.RPL.CatchTargRemov(trNum));
+            end
+            for trNum = 1:size(SPK.TargBotheyes, 2)
+                SPK.TargBotheyes_shifted(:, trNum) = circshift(SPK.TargBotheyes(:, trNum), shift_time - SPK.RPL.TargBotheyes_fake(trNum));
             end
             for trNum = 1:size(SPK.nocatch_disap, 2)
-                SPK.nocatch_disap_shifted(:, trNum) = circshift(SPK.nocatch_disap(:, trNum), 350 - SPK.RPL.nc_disap(trNum));
+                SPK.nocatch_disap_shifted(:, trNum) = circshift(SPK.nocatch_disap(:, trNum), shift_time - SPK.RPL.nc_disap(trNum));
+            end
+            for trNum = 1:size(SPK.nocatch_nodisap, 2)
+                SPK.nocatch_nodisap_shifted(:, trNum) = circshift(SPK.nocatch_nodisap(:, trNum), shift_time - SPK.RPL.nocatch_nodisap_fake(trNum));
             end
             
             save(spk_savename, '-struct', 'SPK', ...
                 'TargBotheyes', 'nocatch_disap', 'CatchTargRemov', 'nocatch_nodisap', ...
-                'CatchTargRemov_shifted', 'nocatch_disap_shifted', ...
+                'CatchTargRemov_shifted', 'TargBotheyes_shifted', ...
+                'nocatch_disap_shifted', 'nocatch_nodisap_shifted', ...
                 'LE_nc_disap', 'RE_nc_disap', 'LE_nc_nodisap', 'RE_nc_nodisap', ...
                 'LE_Physdis', 'RE_Physdis', 'LE_conNodis', 'RE_conNodis', 'all_data', ...
                 'AddInfo', 'RPL')
@@ -154,25 +171,39 @@ for ii = 1:length(sessionNames)
                 BLP.AddInfo = dt.SPKSEL.AddInfo;
                 BLP.RPL = dt.LFPSEL.RPL;
                 
+                % use the same randomized RTs as for SPK data for
+                % corresponding BLP trials
+                BLP.RPL.TargBotheyes_fake = TargBotheyes_fake; % physical condition
+                BLP.RPL.nocatch_nodisap_fake = nocatch_nodisap_fake; % ambiguous condition
+                
                 % create BLP RT-shifted data
                 for trNum = 1:size(BLP.CatchTargRemov, 2)
-                    BLP.CatchTargRemov_shifted(:, trNum) = circshift(BLP.CatchTargRemov(:, trNum), 350 - BLP.RPL.CatchTargRemov(trNum));
+                    BLP.CatchTargRemov_shifted(:, trNum) = circshift(BLP.CatchTargRemov(:, trNum), shift_time - BLP.RPL.CatchTargRemov(trNum));
+                end
+                for trNum = 1:size(BLP.TargBotheyes, 2)
+                    BLP.TargBotheyes_shifted(:, trNum) = circshift(BLP.TargBotheyes(:, trNum), shift_time - BLP.RPL.TargBotheyes_fake(trNum));
                 end
                 for trNum = 1:size(BLP.nocatch_disap, 2)
-                    BLP.nocatch_disap_shifted(:, trNum) = circshift(BLP.nocatch_disap(:, trNum), 350 - BLP.RPL.nc_disap(trNum));
+                    BLP.nocatch_disap_shifted(:, trNum) = circshift(BLP.nocatch_disap(:, trNum), shift_time - BLP.RPL.nc_disap(trNum));
+                end
+                for trNum = 1:size(BLP.nocatch_nodisap, 2)
+                    BLP.nocatch_nodisap_shifted(:, trNum) = circshift(BLP.nocatch_nodisap(:, trNum), shift_time - BLP.RPL.nocatch_nodisap_fake(trNum));
                 end
                 
                 save(blp_savename, '-struct', 'BLP', ...
                     'TargBotheyes', 'nocatch_disap', 'CatchTargRemov', 'nocatch_nodisap', ...
-                    'CatchTargRemov_shifted', 'nocatch_disap_shifted', ...
+                    'CatchTargRemov_shifted', 'TargBotheyes_shifted', ...
+                    'nocatch_disap_shifted', 'nocatch_nodisap_shifted', ...
                     'LE_nc_disap', 'RE_nc_disap', 'LE_nc_nodisap', 'RE_nc_nodisap', ...
                     'LE_Physdis', 'RE_Physdis', 'LE_conNodis', 'RE_conNodis', 'all_data', ...
                     'AddInfo', 'RPL')
-                clear BLP
+                clear BLP 
                 
             end
             
         end
+        
+        clear TargBotheyes_fake nocatch_nodisap_fake
         
         clear dt
         
