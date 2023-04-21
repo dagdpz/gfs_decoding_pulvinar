6011e58ddec98d6e752b0d1f7a309a30bd8265f0
lubavas <19555010+lubavas@users.noreply.github.com>
2023-04-21 11:41:18 +0200

    edited single unit plots
    now spiking frequency is in Hz and MUA power in a.u., edited linestyle and colormap

--- gfs_PNB2023_plot_single_unit_plots.m
+++ gfs_PNB2023_plot_single_unit_plots.m
@@ -9,9 +9,13 @@ params(n).dataFolder = ...
     'C:\Users\lvasileva\Documents\Luba\scripts\ndt_prep\GFS_NIH\';
 params(n).labels = {'disap_physical', 'nodisap_physical', 'disap_ambiguous', 'nodisap_ambiguous'};
 params(n).areas = {'LGN', 'V4', 'pulvinar'};
-params(n).cellIds = {'290707_barney_gfs2-04', '140706_elvis_gfs1-04', '140607_barney_gfs2-04'};
-params(n).channel = [11 36 9];
+params(n).cellIds = {'290707_barney_gfs2-04', '190606_elvis_gfs2-04', '140607_barney_gfs2-04'}; % V4 - 140706_elvis_gfs1-04
+params(n).channel = [11 37 9]; % V4 - 36
 params(n).timing = [1500 3000 5000 6500];
+params(n).multiplier = 1000;
+params(n).ylabel = 'Spiking Frequency, Hz';
+params(n).ylim = [0 90];
+params(n).lineStyle = {':', '-', '--', '-'};
 n = n + 1;
 
 params(n).fileNames = ...
@@ -25,11 +29,19 @@ params(n).areas = {'V1', 'V2', 'V4'};
 params(n).cellIds = {'210304_dali_gfs1', '200304_dali_gfs1', '040404_wally_gfs_2prot'};
 params(n).channel = [11 6 12];
 params(n).timing = [500 800 2200 3200];
+params(n).multiplier = 1;
+params(n).ylabel = 'MUA power, a.u.';
+params(n).ylim = [0 7];
+params(n).lineStyle = {':', '-', '--', '-'};
 
-cmap = cool(4);
+cmap = [0 0 0;
+    0.5 0.5 0.5;
+    0.25 0.73 0.97;
+    0.97 0.41 0.25];
 
 figure,
 set(gcf, 'Position', [1 41 1920 963])
+set(gcf, 'PaperOrientation', 'portrait')
 
 for setNum = 1:2
     
@@ -39,6 +51,7 @@ for setNum = 1:2
         
         load(currFile)
         x_times = binned_site_info.binning_parameters.the_bin_start_times + binned_site_info.binning_parameters.bin_width/2;
+        binned_data = cellfun(@ (x) params(setNum).multiplier*x, binned_data, 'UniformOutput', false);
         
         % choose the needed unit
         currID = cellfun(@ (x) ~isempty(strfind(x, params(setNum).cellIds{flNum})), binned_site_info.info_id);
@@ -68,27 +81,27 @@ for setNum = 1:2
         subplot(2, 3, 3*(setNum - 1) + flNum)
         hold on
         for labNum = 1:4
-%             p(1) = fill([x_times fliplr(x_times) x_times(1)], [upperEB; flipud(lowerEB); upperEB(1)], cmap(labNum, :), 'FaceAlpha', .15, 'EdgeColor', 'none');
-            f(labNum) = fill([x_times fliplr(x_times) x_times(1)], [upperEB_ste(:, labNum); flipud(lowerEB_ste(:, labNum)); upperEB_ste(1, labNum)], cmap(labNum, :), 'FaceAlpha', .2, 'EdgeColor', 'none');
-            p(labNum) = plot(x_times, currMean(:, labNum), 'Color', cmap(labNum, :), 'LineWidth', 2);
+            f(labNum) = ...
+                fill([x_times fliplr(x_times) x_times(1)], [upperEB_ste(:, labNum); flipud(lowerEB_ste(:, labNum)); upperEB_ste(1, labNum)], cmap(labNum, :), 'FaceAlpha', .2, 'EdgeColor', 'none');
+            p(labNum) = ...
+                plot(x_times, currMean(:, labNum), 'Color', cmap(labNum, :), 'LineWidth', 2, 'LineStyle', params(setNum).lineStyle{labNum});
         end
         
         xline(params(setNum).timing(1))
         xline(params(setNum).timing(2))
         xline(params(setNum).timing(3))
         
-        % set lower y-lim to zero
-        y_lim = get(gca, 'YLim');
-        y_lim(1) = 0;
-        set(gca, 'YLim', y_lim);
+        % set y-lims
+        set(gca, 'YLim', params(setNum).ylim);
+        
 %         grid on
         
 %         y_lim_max(labNum) = y_lim(2);
         
         if flNum == 1
-            legend(p, {'Physical Removal', 'Physical Persistence', 'Perceptual Suppression', 'Perceptual Disappearance'}, 'Location', 'Best')
+            legend(p, {'Physical Removal', 'Physical Persistence', 'Perceptual Suppression', 'Perceptual Persistence'}, 'Location', 'Best')
             xlabel('Time after trial start, ms')
-            ylabel('Spiking Frequency, Hz')
+            ylabel(params(setNum).ylabel)
         end
         
         xlim([0 params(setNum).timing(4)])
@@ -101,6 +114,8 @@ for setNum = 1:2
     clear currData currMean upperEB_ste lowerEB_ste
     
 end
-    
 
+set(findall(gcf,'-property','FontSize'),'FontSize',16)
 
+% savename = 'Y:\Posters Presentations Talks\2023_PNM\Luba\single_units.pdf';
+% print(1, '-fillpage', '-dpdf', 'PaperOrientation', 'landscape', savename)
