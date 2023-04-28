73ad14e5135a43b2024e821126295e8d1b42288d
lubavas <19555010+lubavas@users.noreply.github.com>
2023-04-28 15:26:52 +0200

    clean up

--- gfs_lfp_filt.m
+++
@@ -1,8 +0,0 @@
-function [out] = lfp_filt(lfp, low_cut, high_cut)
-
-A = num2cell(lfp, 1); % split all_lfp into rows
-A = cellfun(@(x) bandFiltFilt(x, 'Sampling Frequency', 1000, 'Low Cut', low_cut, 'High Cut', high_cut), A, 'Uniformoutput', 0);
-
-out = [A{:}];
-
-end
